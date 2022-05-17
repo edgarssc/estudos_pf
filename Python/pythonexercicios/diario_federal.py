2802a1cd75905afe6aa704c5ac8086848b5bd1a5
edgarssc <edgarss@gmail.com>
2022-05-17 20:00:02 -0300

    início do meu diário federal

---
+++ Python/pythonexercicios/diario_federal.py
@@ -0,0 +1,6 @@
+#dia 17/05/22
+# hoje eu começo a escrever meu diário, meu objetivo é te-lô como registro da minha jornada de
+# "luto" por desistir da PRF e focar no meu novo objetivo de ser Perito Federal. Tenho muitas dúvidas
+# se tomei a decisão certa, e me pergunto isso todos os dias. Quero me manter focado e acreditando
+# que vai dar certo. Hoje estou mais triste e desanimado que o normal, mas vou focar nos estudos
+# e continuar a acreditar que meu dia vai chegar e que Deus está comigo e sabe que posso vencer.
