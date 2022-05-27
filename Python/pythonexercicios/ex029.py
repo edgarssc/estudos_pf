99d1ab095c5baabc23b902cc2354669ef6d5f495
edgarssc <edgarss@gmail.com>
2022-05-27 20:41:36 -0300

    adicionado cores ao exercício  ex029.py

--- Python/pythonexercicios/ex029.py
+++ Python/pythonexercicios/ex029.py
@@ -1,11 +1,16 @@
 
-velocidade = int(input("Qual a velocidade atual do carro: "))
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+
+velocidade = int(input("{}Qual a velocidade atual do carro:{}".format(cores['amarelo'],cores['limpa'])))
 taxa = 7
 limite = 80
 multa = (velocidade - limite)*taxa
 
 if velocidade <= 40:
-    print("Tenha um bom dia! Dirija com segurança!")
+    print("{}Tenha um bom dia! Dirija com segurança!{}".format(cores['azul'],cores['limpa']))
 elif (velocidade >40 and velocidade <=80):
     print("Você está proximo do limite de velociadade... tenha cuidado:")
 else: 
